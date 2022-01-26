70ce20f52a98bbd4cbde66073c6646ec8b97761f
ArneDeCnodder <r0657784@student.thomasmore.be>
2022-01-26 15:03:13 +0100

    code with push to database

--- code.py
+++ code.py
@@ -1,13 +1,14 @@
+import numpy as np
 import cv2
 from time import sleep
 from time import time
 import pafy
+import requests
 
+largura_min=70 #Largura minima do retangulo
+altura_min=70 #Altura minima do retangulo
 
-largura_min=80 #Largura minima do retangulo
-altura_min=80 #Altura minima do retangulo
-
-offset=6 #Erro permitido entre pixel  
+offset=8 #Erro permitido entre pixel  
 
 pos_linha=550 #Posição da linha de contagem 
 
@@ -16,22 +17,7 @@ delay= 60 #FPS do vídeo
 detec = []
 carros= 0
 
-def createMatrix(rowCount, colCount, dataList):
-    mat = []
-    for i in range(rowCount):
-        rowList = []
-        for j in range(colCount):
-            # you need to increment through dataList here, like this:
-            rowList.append(dataList[rowCount * i + j])
-        mat.append(rowList)
-
-    return mat
-
-def main():
-    alpha = [1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.]
-    mat = createMatrix(5,5,alpha)
-
-	
+    
 def pega_centro(x, y, w, h):
     x1 = int(w / 2)
     y1 = int(h / 2)
@@ -39,7 +25,7 @@ def pega_centro(x, y, w, h):
     cy = y + y1
     return cx,cy
 
-URL = "https://youtu.be/2rt_LmWAO-E" #URL to parse
+URL = "https://www.youtube.com/watch?v=y7QiNgui5Tg" #URL to parse
 play = pafy.new(URL).streams[-1] #'-1' means read the lowest quality of video.
 assert play is not None # we want to make sure their is a input to read.
 cap = cv2.VideoCapture(play.url) #create a opencv video stream.
@@ -47,25 +33,23 @@ cap = cv2.VideoCapture(play.url) #create a opencv video stream.
 subtracao = cv2.bgsegm.createBackgroundSubtractorMOG()
 
 
-
-
 start_time = time()
 print('application start')
 
 while True:
     ret , frame1 = cap.read()
-    tempo = float(1/delay)
-    sleep(tempo) 
+    # tempo = float(1/delay)
+    # sleep(tempo) 
     grey = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(grey,(3,3),5)
     img_sub = subtracao.apply(blur)
-    dilat = cv2.dilate(img_sub,main())
+    dilat = cv2.dilate(img_sub,np.ones((5,5)))
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     dilatada = cv2.morphologyEx (dilat, cv2. MORPH_CLOSE , kernel)
     dilatada = cv2.morphologyEx (dilatada, cv2. MORPH_CLOSE , kernel)
     contorno,h=cv2.findContours(dilatada,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
-    cv2.line(frame1, (25, pos_linha), (600, pos_linha), (255,127,0), 3) 
+    cv2.line(frame1, (1100, 750), (1100, 300), (255,127,0), 3) 
     for(i,c) in enumerate(contorno):
         (x,y,w,h) = cv2.boundingRect(c)
         validar_contorno = (w >= largura_min) and (h >= altura_min)
@@ -84,11 +68,15 @@ while True:
                 # print(tijd.hour)
                 # print(tijd.minute+1)
                 print("There have passed "+str(carros)+" cars in the past 60 seconds")
+                post_url = "https://project40-tm.herokuapp.com/stats/car"
+                myobj = {'amount': carros}
+
+                requests.post(post_url, json = myobj)
                 carros = 0
                 start_time = time()   
-            if y<(pos_linha+offset) and y>(pos_linha-offset) and x>(25+offset) and x<(600-offset):
+            if y<(750+offset) and y>(300-offset) and x>(1100-offset) and x<(1100+offset):
                 carros+=1
-                cv2.line(frame1, (25, pos_linha), (600, pos_linha), (0,127,255), 3)  
+                cv2.line(frame1, (1100, 750), (1100, 300), (0,127,255), 3)  
                 detec.remove((x,y))
                  
        
@@ -99,6 +87,8 @@ while True:
 #     if cv2.waitKey(1) == 27:
 #         break
 
+    
+        
     
 # cv2.destroyAllWindows()
 # cap.release()
