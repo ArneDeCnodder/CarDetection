eee67594817630ee5e46757a43eb36638f540422
ArneDeCnodder <r0657784@student.thomasmore.be>
2022-02-01 10:12:39 +0100

    made the code more user friendly

--- code.py
+++ code.py
@@ -1,15 +1,19 @@
 import numpy as np
 import cv2
-from time import sleep
 from time import time
 import pafy
 import requests
 
-min_contour_width=100  #40
-min_contour_height=100  #40
-offset=5       #10
-line_height=550 #550
+#tweak these settings to get more accurate predictions
+min_contour_width=100  #original = 40 
+min_contour_height=100  # original = 40
+offset=5       # original = 10
+
+
+# define variables and functions
 matches =[]
+start_time = time()
+seconds = 600
 cars=0
 def get_centroid(x, y, w, h):
     x1 = int(w / 2)
@@ -20,17 +24,13 @@ def get_centroid(x, y, w, h):
     return cx,cy
     #return (cx, cy)
 
+# decide what your input will be , here we use a youtube livestream
+
+# cap = cv2.VideoCapture(0) will open the webcam
 URL = "https://www.youtube.com/watch?v=y7QiNgui5Tg" #URL to parse
 play = pafy.new(URL).streams[-1] #'-1' means read the lowest quality of video.
 assert play is not None # we want to make sure their is a input to read.
 cap = cv2.VideoCapture(play.url) #create a opencv video stream.
-# cap = cv2.VideoCapture('video.mp4')   
-#cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture('video.mp4')
-#cap = cv2.VideoCapture('Relaxing highway traffic.mp4')
-
-
-
 
 
 if cap.isOpened():
@@ -40,9 +40,7 @@ else:
 ret,frame1 = cap.read()
 ret,frame2 = cap.read()
 
-
-start_time = time()
-print('application start')
+# detect movement until videostream is over
 
 while ret:
     d = cv2.absdiff(frame1,frame2)
@@ -72,30 +70,23 @@ while ret:
         cv2.circle(frame1,centroid, 5, (0,255,0), -1)
         cx,cy= get_centroid(x, y, w, h)
         for (x,y) in matches:
-            if time() - start_time > 600: # 60 secs
-                # tijd = datetime.now()
-                # print(tijd.hour)
-                # print(tijd.minute+1)
-                # print("There have passed "+str(cars)+" cars in the past 60 seconds")
+
+            # do a post request and send the amount of cars passed in the last .. seconds
+            if time() - start_time > seconds:
                 post_url = "https://project40-tm.herokuapp.com/stats/car"
                 myobj = {'amount': cars}
-
                 requests.post(post_url, json = myobj)
                 cars = 0
-                start_time = time()  
+                start_time = time()
+
+            #if car passed a certain line, increase the counter of cars with one    
             if y<(750+offset) and y>(300-offset) and x>(1100-offset) and x<(1100+offset):
                 cars=cars+1
                 matches.remove((x,y))
-                
-    # cv2.putText(frame1, "Total Cars Detected: " + str(cars), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1,
-    #                 (0, 170, 0), 2)
 
-    # cv2.putText(frame1, "MechatronicsLAB.net", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
-    #                 (255, 170, 0), 2)
-    
-    
+# uncomment the lines below to see what the code does if you run it locally
+
     # cv2.drawContours(frame1,contours,-1,(0,0,255),2)
-
     # cv2.imshow("Original" , frame1)
     # cv2.imshow("Difference" , th)
     # if cv2.waitKey(1) == 27:
